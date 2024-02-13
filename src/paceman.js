01ec23a49d44b029efb9e4e3b3cbdd0cb48f63db
itsdxrk <tanzeemr2@gmail.com>
2024-02-13 20:40:39 +0000

    Testing

---
+++ src/paceman.js
@@ -0,0 +1,36 @@
+let paceman = document.getElementById('paceman');
+let text = document.getElementById('text');
+
+paceman.addEventListener("click", animateonce);
+
+function animateonce() {
+    paceman.classList.add("fadein");
+    getAPIData().then(response => {
+        text.textContent = response[1]["nickname"];
+    }).catch(error => {
+        console.error("Error:", error);
+    })
+    setTimeout(() => {
+        paceman.classList.remove("fadein");
+    }, 1500);
+}
+
+paceman.addEventListener("animationend", postanimation);
+
+function postanimation() {
+    paceman.classList.add("postanim");
+
+    setTimeout(() => {
+        paceman.classList.remove("fadein");
+    }, 1500);
+}
+
+async function getAPIData() {
+    const response = await fetch("https://paceman.gg/api/ars/liveruns", {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        },
+    })
+    return (response.json());
+}
